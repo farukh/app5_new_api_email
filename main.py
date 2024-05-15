84eb97a726b44a09f98ebf893a8447664ea7512d
FarukhMushtaq <farukhmushtaq@apesquared.com>
2024-05-15 16:54:21 +0500

    Improvements: - Limit to 20 news - Language English only - Dynamic Subject/Category in Url - Email Subject - Add News Link.

--- main.py
+++ main.py
@@ -2,14 +2,20 @@ import requests
 from send_email import send_email;
 
 API_KEY = "80fe266478d54a2aa729dde5f78fe974"
-url = "https://newsapi.org/v2/everything?q=tesla&sortby=publishedAt&language=en&apiKey="+API_KEY
+category ="pakistan hockey"
+url = ("https://newsapi.org/v2/everything?"
+       f"q={category}&"
+       "sortby=publishedAt&"
+       "language=en&"
+       "apiKey=")+API_KEY
 
 req = requests.get(url)
 content = req.json()
 content_email = "Subject: News Api Emails\n"
-for article in content['articles']:
+for article in content['articles'][:20]:
     if article['title'] is not None and article['description'] is not None:
-        content_email = content_email + article['title'] + "\n" + article['description'] + 2*"\n"
+        content_email = (content_email + article['title'] + "\n"
+        + article['description'] + article['url'] + 2*"\n")
 
 content_email = content_email.encode('utf-8')
 
