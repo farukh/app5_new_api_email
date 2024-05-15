2f6d36a0cb97b49ff4d064e19afb43e875006175
FarukhMushtaq <farukhmushtaq@apesquared.com>
2024-05-15 13:38:58 +0500

    New API configured and consumed to see Title and Descriptions of news

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import requests
 
 API_KEY = "80fe266478d54a2aa729dde5f78fe974"
-url = "https://newsapi.org/v2/top-headlines/sources?apiKey="+API_KEY
+url = "https://newsapi.org/v2/everything?q=tesla&sortby=publishedAt&language=en&apiKey="+API_KEY
 
 req = requests.get(url)
-content = req.text
-print(content)
+content = req.json()
+
+for article in content['articles']:
+    print(article['title'])
+    print(article['description'])
+
